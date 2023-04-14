df0f594437983ae9f08484d75f732f84ff0902f3
Jhonatan Silva <jhonatanfdvk@gmail.com>
2023-04-13 21:03:48 -0300

    Criado atributos e metodos da classe

---
+++ Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace socialnetwork.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
